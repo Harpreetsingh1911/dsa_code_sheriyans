ad10fd340cec1033a43b89872b75fb02005866fb
Harpreetsingh1911 <hssm1911@gmail.com>
2025-06-03 20:04:37 +0530

    string

--- 5_masterAdvanced_array/arr.js
+++ 5_masterAdvanced_array/arr.js
@@ -1,10 +1,11 @@
-//left rotation by k element with temp Array
-let arr = [1, 2, 3, 4, 5];
-let temArr = new Array(arr.length);
+//!left rotation by k element with temp Array
+// let arr = [1, 2, 3, 4, 5];
+// let temArr = new Array(arr.length);
 
-let k = 3;
-for (let i = 0; i < arr.length; i++) {
-  temArr[i] = arr[(i + k) % arr.length];
-}
-console.log(temArr);
+// let k = 3;
+// for (let i = 0; i < arr.length; i++) {
+//   temArr[i] = arr[(i + k) % arr.length];
+// }
+// console.log(temArr);
 
+//! trapping rain water
