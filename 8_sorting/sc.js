4aa18d6e2dfb47c5ccf596e9269872ef7aabddad
Harpreetsingh1911 <hssm1911@gmail.com>
2025-06-09 19:31:18 +0530

    sorting2

--- 8_sorting/sc.js
+++ 8_sorting/sc.js
@@ -16,20 +16,35 @@
 
 //! selection sort
 
-let arr = [20, 10, 5, 7, 8, 12, 15];
-for (let i = 0; i < arr.length - 1; i++) {
-  let minIndex = i;
-  for (let j = i + 1; j < arr.length; j++) {
-    if (arr[minIndex] > arr[j]) minIndex = j;
-  }
-  if (minIndex != i) {
-    let temp = arr[i];
-    arr[i] = arr[minIndex];
-    arr[minIndex] = temp;
-  }
-}
+// let arr = [20, 10, 5, 7, 8, 12, 15];
+// for (let i = 0; i < arr.length - 1; i++) {
+//   let minIndex = i;
+//   for (let j = i + 1; j < arr.length; j++) {
+//     if (arr[minIndex] > arr[j]) minIndex = j;
+//   }
+//   if (minIndex != i) {
+//     let temp = arr[i];
+//     arr[i] = arr[minIndex];
+//     arr[minIndex] = temp;
+//   }
+// }
 
-console.log(arr);
+// console.log(arr);
 
 //!insertion sort
 
+let arr = [5, 10, 1, 8, 13, 7];
+for (let i = 1; i < arr.length; i++) {
+  debugger;
+  let key = arr[i];
+  let j = i - 1;
+
+  while (j >= 0 && arr[j] > key) {
+    arr[j + 1] = arr[j];
+    j--;
+  }
+
+  arr[j + 1] = key;
+}
+
+console.log(arr);
